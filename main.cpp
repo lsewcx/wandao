5c1bba21057c81d3b0cedab1bf96174f020bd91e
lsewcx <lse210085@163.com>
2023-06-30 10:31:22 +0800

    可以画图上去

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@ cv::Mat fram1;
 
 int main()
 {
+    RoadType roadType = RoadType::BaseHandle; 
     picturepath = "H:/opencvdaima/wandao/1812.jpg"; // 图片路径
     videopath = "H:/opencvdaima/wandao/sample.mp4"; // 视频路径
 
@@ -57,12 +58,44 @@ int main()
         cv::imshow("image123",frame3);
 
 
+         // [04] 出库和入库识别与路径规划
+
+        // [11] 环岛识别与处理
+
+        if (roadType == RoadType::RingHandle ||
+          roadType == RoadType::BaseHandle)
+        {
+            if (ringRecognition.ringRecognition(trackRecognition,frame3))
+            {
+                roadType = RoadType::RingHandle;
+            
+                Mat imageRing =
+                    Mat::zeros(Size(COLSIMAGE, ROWSIMAGE), CV_8UC3); // 初始化图像
+                ringRecognition.drawImage(trackRecognition, imageRing);
+                imshow("imageRecognition", imageRing);
+            }
+            else
+            roadType = RoadType::BaseHandle;
+        }
+
+        trackRecognition.trackRecognition(frame3); // 赛道线识别
+        trackRecognition.drawImage(frame); // 图像显示赛道线识别结果
         controlCenterCal.controlCenterCal(trackRecognition); // 根据赛道边缘信息拟合运动控制中心
         controlCenterCal.drawImage(trackRecognition, frame);
-        trackRecognition.trackRecognition(frame3);
-        trackRecognition.drawImage(frame); // 图像显示赛道线识别结果
         imshow("imageTrack", frame);
-
+        switch (roadType)
+        {
+            case RoadType::BaseHandle: // 基础赛道处理 // 基础赛道处理
+                putText(frame, "[1] Track", Point(10, 20),
+                        cv::FONT_HERSHEY_TRIPLEX, 0.3, cv::Scalar(0, 0, 255), 1,
+                        CV_AA); // 显示赛道识别类型
+                break;
+            case RoadType::RingHandle: // 环岛赛道处理 // 环岛赛道处理
+                putText(frame, "[2] Ring", Point(10, 20),
+                        cv::FONT_HERSHEY_TRIPLEX, 0.3, cv::Scalar(0, 255, 0), 1,
+                        CV_AA); // 显示赛道识别类型
+                break;
+        }
 
 
         char key = waitKey(10);//读取视频
@@ -71,5 +104,4 @@ int main()
             break;
         }
     }
-    return 0;
 }
--- public.hpp
+++ public.hpp
@@ -10,3 +10,19 @@ PathSearching path1;
 RingRecognition  ringRecognition;
 TrackRecognition trackRecognition;
 ControlCenterCal controlCenterCal;
+
+
+
+enum RoadType
+{
+  BaseHandle = 0, // 基础赛道处理
+  RingHandle,     // 环岛赛道处理
+  CrossHandle,    // 十字道路处理
+  FreezoneHandle, // 泛行区处理
+  GarageHandle,   // 车库处理
+  GranaryHandle,  // 粮仓处理
+  DepotHandle,    // 修车厂处理
+  BridgeHandle,   // 坡道(桥)处理
+  SlowzoneHandle, // 慢行区（动物出没）处理
+  FarmlandHandle, // 农田区域处理
+};
