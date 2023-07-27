8542b9def2d73da59826abddee95ec4729fb07b9
lsewcx <lse210085@163.com>
2023-07-27 09:13:34 +0800

    虚函数使用方法

---
+++ virq.cpp
@@ -0,0 +1,37 @@
+/**
+ * @author lse
+ * @note 虚函数使用方法用来重写另一段class中的方法
+ **/
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+class bass
+{
+public:
+    virtual void abc()
+    {
+        int a=0;
+        a++;
+        std::cout<<a<<endl;
+    }
+};
+class deas : public bass
+{
+public:
+    void abc() override
+    {
+        int b=0;
+        b=8;
+        std::cout<<b<<endl;
+    }
+};
+int main()
+{
+    bass *xuhanshu = new deas();
+    xuhanshu->abc();
+    delete xuhanshu;
+    system("pause");
+    return 0;
+}
